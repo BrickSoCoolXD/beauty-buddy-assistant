958b63e9010f1409f40ea319cca3f2824d6fc8d0
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-10-19 10:48:03 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = ({ onCartClick }) => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
-            <img className="h-8 w-auto" src="https://www.flaticon.com/free-icon/cosmetic_3906447" alt="Bosar Cosmetics" />
+            <img className="h-8 w-auto" src="https://www.flaticon.com/free-icon/cosmetic_3906447.png" alt="Bosar Cosmetics" />
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
             <a href="#" className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
